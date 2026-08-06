9b093aa3dd037bba5dac6b2cdfb4515700914dea
abdulla641 <91000146+abdulla641@users.noreply.github.com>
2026-08-06 11:32:15 +0300

    Add credit card and payment tracking logic

--- credit-tracker/script.js
+++ credit-tracker/script.js
@@ -1,6 +1,8 @@
 let data=JSON.parse(localStorage.getItem('creditData'))||[];
 function save(){localStorage.setItem('creditData',JSON.stringify(data));render();}
-function addReceive(){let n=receiveName.value,a=Number(receiveAmount.value),c=receiveCurrency.value;if(n&&a)data.push({type:'receive',name:n,amount:a,currency:c});save();}
-function addPay(){let n=payName.value,a=Number(payAmount.value),c=payCurrency.value;if(n&&a)data.push({type:'pay',name:n,amount:a,currency:c});save();}
-function render(){let list=document.getElementById('list'),qRec=0,qPay=0,iRec=0,iPay=0;list.innerHTML='';data.forEach(x=>{if(x.type==='receive'){x.currency==='QAR'?qRec+=x.amount:iRec+=x.amount}else{x.currency==='QAR'?qPay+=x.amount:iPay+=x.amount}list.innerHTML+=`<div class='card ${x.type}'><b>${x.name}</b><br>${x.amount} ${x.currency}</div>`});dashboard.innerHTML=`🟢 Receive: ${qRec} QAR | ${iRec} INR<br>🔴 Pay: ${qPay} QAR | ${iPay} INR`;}
+function addReceive(){addItem('receive',receiveName.value,receiveAmount.value,receiveCurrency.value)}
+function addPay(){addItem('pay',payName.value,payAmount.value,payCurrency.value)}
+function addCard(){addItem('card',cardName.value,cardAmount.value,cardCurrency.value)}
+function addItem(type,name,amount,currency){if(name&&amount){data.push({type,name,amount:Number(amount),currency,date:new Date().toLocaleDateString()});save();}}
+function render(){let list=document.getElementById('list');let qr=0,qi=0,qp=0,qpi=0,cc=0;list.innerHTML='';data.forEach(x=>{if(x.type==='receive'){x.currency==='QAR'?qr+=x.amount:qi+=x.amount}else if(x.type==='pay'){x.currency==='QAR'?qp+=x.amount:qpi+=x.amount}else{cc+=x.amount}list.innerHTML+=`<div class="card ${x.type}"><b>${x.name}</b><br>${x.amount} ${x.currency}<br>${x.date}</div>`});dashboard.innerHTML=`<div class="dash">🟢 Receive: ${qr} QAR | ${qi} INR</div><div class="dash">🔴 Pay: ${qp} QAR | ${qpi} INR</div><div class="dash">💳 Cards: ${cc}</div>`}
 render();
