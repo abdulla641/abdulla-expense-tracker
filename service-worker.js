b72bc2675427e8655c4e96ad7ae89f8965286b96
abdulla641 <91000146+abdulla641@users.noreply.github.com>
2026-08-06 16:24:46 +0300

    Force refresh finance app cache version

--- service-worker.js
+++ service-worker.js
@@ -1,121 +1,34 @@
-// Abdulla Expense Tracker
-// Offline Service Worker
-
-
-const CACHE_NAME = "abdulla-expense-v1";
-
+// Abdulla Finance Service Worker
+const CACHE_NAME = "abdulla-finance-v12";
 
 const FILES_TO_CACHE = [
-
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
-
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
-
-
-
-// Install
-
-self.addEventListener(
-"install",
-event => {
-
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>{
-
-return cache.addAll(
-FILES_TO_CACHE
-);
-
-})
-
-);
-
-
-self.skipWaiting();
-
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
-
-
-
-
-// Activate
-
-self.addEventListener(
-"activate",
-event=>{
-
-
-event.waitUntil(
-
-caches.keys()
-.then(keys=>{
-
-
-return Promise.all(
-
-keys.map(key=>{
-
-
-if(
-key !== CACHE_NAME
-){
-
-return caches.delete(key);
-
-}
-
-
-})
-
-
-);
-
-
-})
-
-
-);
-
-
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
-
-
-
-
-// Offline loading
-
-self.addEventListener(
-"fetch",
-event=>{
-
-
-event.respondWith(
-
-
-caches.match(event.request)
-
-.then(response=>{
-
-
-return response || fetch(event.request);
-
-
-})
-
-
-);
-
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
